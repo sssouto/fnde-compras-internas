ceec9b987c047019bf3d8032b84de5807b68043c
sander souto <sander.soares@yahoo.com.br>
2021-01-13 00:32:39 -0300

    actions and views processos new and create

--- app/controllers/processos_controller.rb
+++ app/controllers/processos_controller.rb
@@ -1,4 +1,6 @@
 class ProcessosController < ApplicationController
+  before_action :set_processo, only: %i[show edit update]
+
   def index
   end
 
@@ -6,14 +8,34 @@ class ProcessosController < ApplicationController
   end
 
   def new
+    @processo = Processo.new
   end
 
   def create
+    @processo = Processo.new(processo_params)
+    if @processo.save
+      # redirect_to processos_path
+      redirect_to processo_path(@processo), notice: 'O processo foi criado.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @processo.update(processo_params)
+    redirect_to processo_path(@processo), notice: 'O processo foi atualizado.'
+  end
+
+  private
+
+  def set_processo
+    @processo = Processo.find(params[:id])
+  end
+
+  def processo_params
+    params.require(:processo).permit(:sei, :objeto, :demandante, :tipo_contratacao, :fase)
   end
 end
--- app/models/processo.rb
+++ app/models/processo.rb
@@ -1,3 +1,6 @@
 class Processo < ApplicationRecord
   has_many :itens
+  DEMANDANTE = ["CGLOG/DIRAD", "CGPEO/DIRAD", "CGCOM/DIRAD"]
+  TIPO = ["Pregão", "Dispensa de licitação", "Inexigibilidade", "Adesão ARP"]
+  FASE = ["Tramitação inicial", "ETP/Mapa de riscos", "TR/PB", "Instrução pré-PROFE", "PROFE", "Instrução pós-PROFE", "Fase externa-SIASG/Comprasnet", "Autoridade competente/empenho", "Contrato em execução", "Processo encerrado"]
 end
